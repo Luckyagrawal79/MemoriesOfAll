723fada8273811a207a4422ddbf026267c26d638
Lucky <luckyagarwal79@gmail.com>
2023-07-25 05:46:36 +0530

    done

--- server/index.js
+++ server/index.js
@@ -4,36 +4,14 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app = express();
 
-// Function to extract the client IP address from the request headers
-function getClientIp(req) {
-  // First, check the 'X-Forwarded-For' header
-  const xForwardedFor = req.header("X-Forwarded-For");
-  if (xForwardedFor) {
-    return xForwardedFor.split(",")[0].trim();
-  }
-
-  // If 'X-Forwarded-For' is not available, use 'req.connection.remoteAddress'
-  return req.connection.remoteAddress;
-}
-
-// Use custom keyGenerator function to extract client IP for rate limiting
-app.use(
-  rateLimit({
-    windowMs: 12 * 60 * 1000, // 1 minute
-    max: 30,
-    keyGenerator: (req) => getClientIp(req), // Use the custom function to get the client IP
-  })
-);
-
 app.use(cors());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/posts", PostRoutes);
 app.use("/user", UserRoutes);
