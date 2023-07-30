195b7c47d554402fda9ea347c993909eb21461ab
Lucky <luckyagarwal79@gmail.com>
2023-07-30 20:31:39 +0530

    limit changed

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/posts", PostRoutes);
 app.use("/user", UserRoutes);
